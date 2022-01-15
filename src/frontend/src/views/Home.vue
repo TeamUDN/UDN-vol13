d43afec33b3a5cac6f122101156a6e5eccc66645
Yuuna Yanagida <66319933+y-yana@users.noreply.github.com>
2022-01-15 16:01:54 +0900

    show chart success!!!!!!

--- src/frontend/src/views/Home.vue
+++ src/frontend/src/views/Home.vue
@@ -1,11 +1,17 @@
 <template>
   <div class="home">
     <h1>This is a home page</h1>
+    <h2>chart sample</h2>
+    <Chart></Chart>
   </div>
 </template>
 
 <script>
+import Chart from './Chart'
 export default {
-  name: 'home'
+  name: 'home',
+  components: {
+    Chart
+  }
 }
 </script>
