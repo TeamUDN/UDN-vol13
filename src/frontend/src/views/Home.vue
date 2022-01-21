53fd8896b3146679f92a7625e49e5cca595b8055
Yuuna Yanagida <66319933+y-yana@users.noreply.github.com>
2022-01-21 15:06:39 +0900

    top page layout

--- src/frontend/src/views/Home.vue
+++ src/frontend/src/views/Home.vue
@@ -1,20 +1,26 @@
 <template>
   <div class="home">
-    <!--<h1>This is a home page</h1>
-    <h2>chart sample</h2>-->
     <div class="allChartArea">
       <div class="chartArea">
         <p>全体</p>
-        <Chart canvas-label-type="date" label-end-num="9"></Chart>
+        <div>
+          <Chart canvas-label-type="date" label-end-num="9"></Chart>
+        </div>
       </div>
       <div class="chartArea">
         <p>チーム</p>
-        <Chart canvas-label-type="time" label-end-num="15"></Chart>
+        <div>
+          <Chart canvas-label-type="time" label-end-num="15"></Chart>
+        </div>
       </div>
       <div class="chartArea">
         <p>個人</p>
-        <Chart canvas-label-type="time" label-end-num="30"></Chart>
+        <div>
+          <Chart canvas-label-type="time" label-end-num="30"></Chart>
+        </div>
       </div>
+    </div>
+    <div class="tabArea">
     </div>
   </div>
 </template>
@@ -29,11 +35,33 @@ export default {
 }
 </script>
 
-<style scoped>
+<style lang="scss" scoped>
+.home {
+  display: flex;
+}
 .allChartArea {
-  width: 40rem;
+  width: 75%;
   display: flex;
   flex-direction: column;
-  gap: 4rem;
+}
+.chartArea{
+  &:nth-child(even){
+    background-color: #FFEAEF;
+  }
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  padding: 1rem 0 4rem 0;
+  p{
+    font-size: 1.7rem;
+  }
+  div {
+    width: 50rem;
+  }
+}
+.tabArea {
+  width: 25%;
+  background-color: gray;
 }
 </style>
