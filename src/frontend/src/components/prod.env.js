8ae79f1b287a00ab66c54656ca9c1582039958e6
Leon Kunishi <49759672+Kamenleon@users.noreply.github.com>
2022-01-22 22:52:26 +0900

    test

--- src/frontend/src/components/prod.env.js
+++ src/frontend/src/components/prod.env.js
@@ -2,6 +2,28 @@ import 'firebase/compat/firestore'
 import 'firebase/compat/auth'
 import 'firebase/compat/storage'
 
+/*
+const aws = require('aws-sdk');
+let s3 = new aws.S3({
+  accessKeyId: process.env.S3_KEY,
+  secretAccessKey: process.env.S3_SECRET
+});
+*/
+/*
+const vueEnv = {
+  NODE_ENV: '"production"',
+  REST_API_URL: JSON.stringify(process.env.REST_API_URL),
+  VUE_APP_API_KEY: JSON.stringify(process.env.VUE_APP_API_KEY),
+  VUE_APP_APP_ID: JSON.stringify(process.env.VUE_APP_APP_ID),
+  VUE_APP_AUTH_DOMAIN: JSON.stringify(process.env.VUE_APP_AUTH_DOMAIN),
+  VUE_APP_DATABASE_URL: JSON.stringify(process.env.VUE_APP_DATABASE_URL),
+  VUE_APP_MEASUREMENT_ID: JSON.stringify(process.env.VUE_APP_MEASUREMENT_ID),
+  VUE_APP_MESSAGING_SENSOR_ID: JSON.stringify(process.env.VUE_APP_MESSAGING_SENSOR_ID),
+  VUE_APP_PROJECT_ID: JSON.stringify(process.env.VUE_APP_PROJECT_ID),
+  VUE_APP_STORAGE_BUCKET: JSON.stringify(process.env.VUE_APP_STORAGE_BUCKET),
+  VUE_APP_API_TEST: JSON.stringify(process.env.VUE_APP_API_TEST)
+}
+*/
 const vueEnv = {
   NODE_ENV: '"production"',
   REST_API_URL: JSON.stringify(process.env.REST_API_URL),
